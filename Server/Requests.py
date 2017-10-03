9c56747ab24934839f3156d7824035982934467f
Ubuntu <ubuntu@ip-172-31-29-224.us-west-2.compute.internal>
2017-10-03 00:00:24 +0000

    start adding crypto

--- Server/Requests.py
+++ Server/Requests.py
@@ -5,6 +5,21 @@ def send(conn, message):
     message = str(total_length) + '#' + message
     conn.sendall(message.encode())
 
+def recv(conn):
+    firstchunk = conn.recv(16).decode()
+    if firstchunk:
+        print(firstchunk)
+        received = len(firstchunk)
+        content = ''
+        length, chunk = firstchunk.split('#')
+        content += chunk
+        while received < int(length):
+            nextchunk = conn.recv(16).decode()
+            received += len(nextchunk)
+            content += nextchunk
+        return content
+    return None
+
 def register(conn, content):
     #TODO generate salt, hash salt + name, hash salt + pwd, store in db 
     print("register!")
@@ -15,7 +30,7 @@ def register(conn, content):
     print("sent response")
     
 
-def receive(conn, content):
+def send_message(conn, content):
     #TODO store in message db
     pass
 
